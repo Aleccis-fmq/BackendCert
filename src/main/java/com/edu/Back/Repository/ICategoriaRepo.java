4c02a3f609329d6b89b678c6610533a7225b8af7
“Aleccis.Windows” <alexis-fl-live@outlook.com>
2024-02-19 14:00:53 -0500

    Raiz main : Modelo/Repository,Service,ServiceImpl,Controller VERSION 1, NIVEL 1

--- src/main/java/com/edu/Back/Repository/ICategoriaRepo.java
+++ src/main/java/com/edu/Back/Repository/ICategoriaRepo.java
@@ -1,7 +1,6 @@
 package com.edu.Back.Repository;
 
 import com.edu.Back.Model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICategoriaRepo extends IGenericRepo<Categoria,Integer>{
 }
